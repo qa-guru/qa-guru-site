4d9611af1d429df618df7da38161d78e76021060
Nik Elin <madanes07051999@icloud.com>
2022-12-01 17:37:10 +0200

    fix styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
 import "./i18n/config";
-import "./styles/global/index.scss";
+import "./styles/index.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
