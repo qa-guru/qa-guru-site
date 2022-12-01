a8d2d0c6f2e29abb95de78760698f8173d36b5e4
Bragin Boris <bragin.boris@ya.ru>
2022-12-02 02:39:55 +0300

    Fix layout path to Header

--- src/shared/ui/Layout/Layout.tsx
+++ src/shared/ui/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Box from "@mui/material/Box";
-import Header from "../../../components/Header /Header";
+import Header from "../../../components/Header/Header";
 import Footer from "../../../components/Footer/Footer";
 import { Outlet } from "react-router-dom";
 import styles from "./Layout.module.scss";
